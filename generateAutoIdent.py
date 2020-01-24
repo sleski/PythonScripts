272f1be3f728213369c2293b05105b12ee5493ba
Slawomir Leski <slawomir.leski@gmail.com>
2020-01-24 13:37:52 +0100

    Updated.

--- generateAutoIdent.py
+++ generateAutoIdent.py
@@ -2,3 +2,8 @@
 
 # select shortname, select env and create token
 
+import urllib, json
+url = "http://maps.googleapis.com/maps/api/geocode/json?address=google"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print data
